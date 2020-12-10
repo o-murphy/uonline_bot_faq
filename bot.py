d0e8c9be18e5b18c34ab248327dffb493630ef08
o-murphy <thehelixpg@gmail.com>
2020-12-10 11:58:44 +0200

    added command /sup fixed some bugs

--- bot.py
+++ bot.py
@@ -31,12 +31,14 @@ def start(message):
                      reply_markup=kb.start(), parse_mode='HTML')
 
 
-@bot.message_handler(commands=['start', 'faq', 'help', 'contact'])
+@bot.message_handler(commands=['start', 'faq', 'help', 'contact', 'sup'])
 def faq_cmd(message):
     if message.text == '/start':
         start(message)
     elif message.text == '/faq':
         menu_faq(message)
+    elif message.text == '/sup':
+        sup(message)
     else:
         info.run(bot, message, message.text)
 
@@ -93,7 +95,7 @@ def support_reply_handler(message):
 @bot.message_handler(content_types='text', func=lambda message: message.chat.id != admin_group)
 def get_question(message):
     data = ask.classify_question(message.text)
-    dump_data(message.from_user, message.text, ptype='MESSAGE')
+    dump_data(message.from_user, message.chat, message.text, ptype='MESSAGE')
     msg_text = f'<b>Скорее всего вы имели ввиду:</b>\n\n{data["answer"]}'
     try:
         bot.send_message(message.from_user.id, msg_text,
@@ -103,10 +105,10 @@ def get_question(message):
         print(exc)
 
 
-@bot.callback_query_handler(lambda call: re.search('q_id=', call.data))
+@bot.callback_query_handler(lambda call: re.search(r'q_id=', call.data))
 def get_answer(call):
     bot.answer_callback_query(call.id)
-    q_id = int(re.sub('q_id=', '', call.data))
+    q_id = int(re.sub(r'q_id=', '', call.data))
     msg = ask.answer(q_id)
     photo = ask.pic(q_id)
     if not photo:
@@ -123,28 +125,37 @@ def get_answer(call):
         print(exc)
 
 
-def sup(call):
-    dump_data(call.message.from_user, 'sup callback', ptype='CALL')
-    bot.send_message(call.from_user.id,
+def sup(data):
+    try:
+        from_user = data.from_user
+        chat = data.message.chat
+        msg = data.message
+    except Exception as exc:
+        print(exc)
+        from_user = data.from_user
+        chat = data.chat
+        msg = data
+    dump_data(from_user, chat, 'sup callback', ptype='CALL')
+    bot.send_message(data.from_user.id,
                      'Введите ваш номер телефона в формате +380123456789'
                      ' или нажмите "Отправить контакт"',
                      reply_markup=kb.phone())
-    bot.register_next_step_handler(call.message, get_phone)
+    bot.register_next_step_handler(msg, get_phone)
 
 
 def get_phone(message):
     if message.text == 'Отмена':
-        dump_data(message.from_user, 'Cancel', ptype='CANCEL')
+        dump_data(message.from_user, message.chat, 'Cancel', ptype='CANCEL')
         bot.clear_step_handler(message)
         bot.send_message(message.chat.id,
                          'Регистрация заявки отменена!',
                          reply_markup=kb.remove())
         start(message)
     else:
-        dump_data(message.from_user, message.contact.phone_number, ptype='CONTACT')
         phone_number = phone.get(message)
         is_valid = phone.check(phone_number)
         if is_valid and len(list(phone_number)) == 13:
+            dump_data(message.from_user, message.chat, phone_number, ptype='CONTACT')
             bot.send_message(message.from_user.id,
                              'Коротко опишите Ваш запрос',
                              reply_markup=kb.cancel())
@@ -159,22 +170,23 @@ def get_phone(message):
 
 def register(message, phone_number):
     if message.text == 'Отмена':
-        dump_data(message.from_user, 'Cancel', ptype='CANCEL')
+        dump_data(message.from_user, message.chat, 'Cancel', ptype='CANCEL')
         bot.clear_step_handler(message)
         bot.send_message(message.chat.id,
                          'Регистрация заявки отменена!',
                          reply_markup=kb.remove())
         start(message)
     else:
-        dump_data(message.from_user, message.text, ptype='TASK CREATE')
+
         task = f'#TASK{message.chat.id}_{message.message_id}'
         bot.forward_message(admin_group,
                             message.chat.id,
                             message.message_id,
                             disable_notification=True)
-        bot.send_message(admin_group,
-                         f'{task}\n{phone_number}',
-                         reply_markup=kb.make_close())
+        msg = bot.send_message(admin_group,
+                               f'{task}\n{phone_number}',
+                               reply_markup=kb.make_close())
+        dump_data(message.from_user, msg.chat, message.text, ptype='TASK CREATE')
         bot.send_message(message.from_user.id,
                          f'{task}\nЗарегистрирована в системе поддержки',
                          reply_markup=kb.remove())
@@ -186,21 +198,28 @@ def tasks(call):
         bot.edit_message_reply_markup(call.message.chat.id,
                                       call.message.message_id,
                                       reply_markup=kb.make_open())
+        dump_data(call.from_user, call.message.chat, 'Manually closed', ptype='TASK CLOSE')
         bot.answer_callback_query(call.id, 'Manually closed')
     else:
         bot.edit_message_reply_markup(call.message.chat.id,
                                       call.message.message_id,
                                       reply_markup=kb.make_close())
+        dump_data(call.from_user, call.message.chat, 'Manually opened', ptype='TASK OPEN')
         bot.answer_callback_query(call.id, 'Manually opened')
 
 
-def dump_data(user, question, ptype='INFO'):
-    username = user.username
-    first_name = user.first_name
-    uid = user.id
+def dump_data(from_user, to_chat, text, ptype='INFO'):
     now = datetime.now().strftime('%d.%m.%y %H:%M:%S')
-    csv = f"{now};{ptype};{username};{first_name};{uid};{question}\n"
-    tsv = f"[{now}]\t{ptype}:\t{username}\t{first_name}\t{uid}\t{question}\n"
+    username = from_user.username
+    first_name = from_user.first_name
+    uid = from_user.id
+    to_id = to_chat.id
+    if to_chat.username:
+        to_chat_name = to_chat.username
+    else:
+        to_chat_name = to_chat.title
+    csv = f"{now};{ptype};{username};{first_name};{uid};{to_chat_name};{to_id};{text}\n"
+    tsv = f"[{now}]\t{ptype}:\t{username}\t{first_name}\t{uid}\t{to_chat_name}\t{to_id}\t{text}\n"
     print(tsv)
     with open("dump.csv", "a") as f:
         f.write(csv)
@@ -212,7 +231,7 @@ if __name__ == '__main__':
     print('bot started!\npolling..')
     while True:
         try:
-            # bot.send_message(admin_group, 'bot started!\npolling..')
+            bot.send_message(admin_group, 'bot started!\npolling..')
             bot.polling()
         except Exception as e:
             try:
--- modules/support.py
+++ modules/support.py
@@ -1,11 +1,16 @@
-from bot import bot
+from bot import bot, dump_data
 import re
 
 
 def reply(message):
-    reply_to_text = message.reply_to_message.text
-    task_id = re.sub('#TASK', '', reply_to_text)
-    chat_id = int(re.search(r'^\d+', task_id).group())
-    text = re.sub(r'\+\d+$', '', reply_to_text)
-    bot.send_message(chat_id, f'Ответ на заявку:\n{text}')
-    bot.forward_message(chat_id, message.chat.id, message.message_id)
+    if re.search(r'^#TASK', message.reply_to_message.text):
+        reply_to_text = message.reply_to_message.text
+        task_id = re.sub('#TASK', '', reply_to_text)
+        chat_id = int(re.search(r'^\d+', task_id).group())
+        text = re.sub(r'\+\d+$', '', reply_to_text)
+        msg = bot.send_message(chat_id, f'Ответ на заявку:\n{text}')
+        bot.forward_message(chat_id, message.chat.id, message.message_id)
+        bot.reply_to(message, 'Ответ успешно доставлен')
+        dump_data(message.from_user, msg.chat, message.text, ptype='TASK REPLY')
+    else:
+        bot.reply_to(message, 'Ответ необходимо дать на сообщение с номером заявки!')
